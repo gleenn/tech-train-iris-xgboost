d92dcda869cc7baa7256c17dec01c40aa78b784b
Glenn Jahnke <gleenn@apple.com>
2024-01-19 23:18:00 -0600

    broken :/

--- test/tech_train_iris_xgboost/core_test.clj
+++ test/tech_train_iris_xgboost/core_test.clj
@@ -4,7 +4,9 @@
             [tech.v3.dataset :as ds]
             [tech.v3.dataset.categorical :as ds-cat]
             [tech.v3.dataset.column-filters :as cf]
-            [tech.v3.dataset.modelling :as ds-mod]))
+            [tech.v3.dataset.modelling :as ds-mod]
+            [tech.v3.libs.xgboost]
+            [tech.v3.ml :as ml]))
 
 ;; blatantly stolen from https://github.com/techascent/tech.ml
 
@@ -36,3 +38,27 @@
          (-> (meta (numeric-ds "species"))
              (update :categorical-map #(into {} %))))))
 
+(def regression-ds (ds-mod/set-inference-target numeric-ds "petal_width"))
+
+(def model (ml/train-split regression-ds {:model-type :xgboost/regression}))
+
+(deftest loss-test
+  (prn :loss (:loss model))
+  (is (< 0.0 (:loss model) 1.0)))
+
+(try
+  (def k-fold-model (ml/train-k-fold regression-ds {:model-type :xgboost/regression}))
+
+  ;;; WTF!
+  ;;; => java.lang.ClassCastException: class java.lang.Double cannot be cast to class clojure.lang.IFn
+  (catch Exception e (.printStackTrace e)))
+
+
+#_(select-keys k-fold-model [:min-loss :avg-loss :max-loss])
+
+#_(def predictions (ml/predict numeric-ds k-fold-model))
+
+#_(prn :predictions
+     (-> predictions
+         ds-cat/reverse-map-categorical-xforms
+         ds/head))
