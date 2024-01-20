dcbd5a1a3018f51061fcec017d636c01d7b9c2ae
Glenn Jahnke <gleenn@apple.com>
2024-01-19 22:10:17 -0600

    stuff

--- test/tech_train_iris_xgboost/core_test.clj
+++ test/tech_train_iris_xgboost/core_test.clj
@@ -1,7 +1,22 @@
 (ns tech-train-iris-xgboost.core-test
   (:require [clojure.test :refer :all]
-            [tech-train-iris-xgboost.core :refer :all]))
+            [tech-train-iris-xgboost.core :refer :all]
+            [tech.v3.dataset :as ds]
+            [tech.v3.dataset.column-filters :as cf]
+            [tech.v3.dataset.modelling :as ds-mod]))
+
+
+(def ds (ds/->dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv"))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (is (= 5 (count (ds/head ds)))))
+
+;;; print only the numerical fields in the data set
+(prn :numeric (ds/head (cf/numeric ds)))
+
+;;; print only the categorical fields in the data set
+(prn :categorical (ds/head (cf/categorical ds)))
+
+;;; convert the categorical fields to numeric for regression
+(def numeric-ds (ds/categorical->number ds cf/categorical))
+
